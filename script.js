774385539fccaaaf2731863b31448a968d32ed1c
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-03-10 22:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  let subarrays = [];
+  let currentSubarray = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      subarrays.push(currentSubarray);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
+
+  // Append the last subarray
+  if (currentSubarray.length > 0) {
+    subarrays.push(currentSubarray);
+  }
+
+  return subarrays;
 };
 
-const n = prompt("Enter n: ");
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const n = parseInt(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
